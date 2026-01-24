6ea11559991d0bb376b3173c14e7fbb65c592a58
lyncorbett <Lyn@thepivotalgroup.com>
2026-01-23 22:12:26 -0800

    Create page.jsx

---
+++ nonprofit-edge-deploy/src/app/admin/emails/page.jsx
@@ -0,0 +1,8 @@
+// File: src/app/admin/emails/page.jsx
+// Route: /admin/emails
+
+import EmailApprovalDashboard from '@/components/EmailApprovalDashboard';
+
+export default function EmailsPage() {
+  return <EmailApprovalDashboard />;
+}
