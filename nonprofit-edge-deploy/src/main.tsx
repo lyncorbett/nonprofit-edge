4659992f41dcf445b74a2f29dc61925278cd6a69
lyncorbett <Lyn@thepivotalgroup.com>
2026-02-06 18:16:07 -0800

    Wire up PWA install banner in App.tsx

--- nonprofit-edge-deploy/src/main.tsx
+++ nonprofit-edge-deploy/src/main.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './App'
 import './index.css'
+import { registerServiceWorker, setupInstallPrompt } from './registerSW'
+
+// Register PWA service worker
+registerServiceWorker()
+setupInstallPrompt()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
